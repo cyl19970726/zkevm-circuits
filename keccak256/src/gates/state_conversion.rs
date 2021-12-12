c356bb4eff84628fac3c62077981f3a7541d11e4
ChihChengLiang <chihchengliang@gmail.com>
2021-12-12 21:26:07 +0800

    add from base 9 state config

--- keccak256/src/gates/state_conversion.rs
+++ keccak256/src/gates/state_conversion.rs
@@ -4,7 +4,7 @@ use halo2::{
 };
 
 use crate::gates::base_conversion::BaseConversionConfig;
-use crate::gates::tables::FromBinaryTableConfig;
+use crate::gates::tables::{FromBase9TableConfig, FromBinaryTableConfig};
 use pairing::arithmetic::FieldExt;
 use std::convert::TryInto;
 
@@ -37,6 +37,58 @@ impl<F: FieldExt> FromBinaryState<F> {
             bccs,
             state,
             output_b9,
+        }
+    }
+
+    pub fn assign_region(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        input_state: [F; 25],
+    ) -> Result<[F; 25], Error> {
+        let output_state: Result<Vec<F>, Error> = input_state
+            .iter()
+            .zip(self.bccs.iter())
+            .map(|(&lane, config)| {
+                let output = config.assign_region(layouter, lane)?;
+                Ok(output)
+            })
+            .into_iter()
+            .collect();
+        let output_state = output_state?;
+        let output_state: [F; 25] = output_state.try_into().unwrap();
+        Ok(output_state)
+    }
+}
+
+struct FromBase9State<F> {
+    table: FromBase9TableConfig<F>,
+    bccs: [BaseConversionConfig<F>; 25],
+    state: [Column<Advice>; 25],
+    output_b2: bool,
+}
+
+impl<F: FieldExt> FromBase9State<F> {
+    fn configure(
+        meta: &mut ConstraintSystem<F>,
+        state: [Column<Advice>; 25],
+        table: FromBase9TableConfig<F>,
+        output_b2: bool,
+    ) -> Self {
+        let bi = table.get_base_info(output_b2);
+        let bccs: [BaseConversionConfig<F>; 25] = state
+            .iter()
+            .map(|&lane| {
+                BaseConversionConfig::configure(meta, bi.clone(), lane)
+            })
+            .collect::<Vec<_>>()
+            .try_into()
+            .unwrap();
+
+        Self {
+            table,
+            bccs,
+            state,
+            output_b2,
         }
     }
 
