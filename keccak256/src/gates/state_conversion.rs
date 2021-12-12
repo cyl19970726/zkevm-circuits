48a60c87e84f3eed1af19a9227c544255d9a45a5
ChihChengLiang <chihchengliang@gmail.com>
2021-12-13 00:53:40 +0800

    simplify

--- keccak256/src/gates/state_conversion.rs
+++ keccak256/src/gates/state_conversion.rs
@@ -4,25 +4,22 @@ use halo2::{
 };
 
 use crate::gates::base_conversion::BaseConversionConfig;
-use crate::gates::tables::{FromBase9TableConfig, FromBinaryTableConfig};
+use crate::gates::tables::BaseInfo;
 use pairing::arithmetic::FieldExt;
 use std::convert::TryInto;
 
-struct FromBinaryState<F> {
-    table: FromBinaryTableConfig<F>,
+struct StateBaseConversion<F> {
+    bi: BaseInfo<F>,
     bccs: [BaseConversionConfig<F>; 25],
     state: [Column<Advice>; 25],
-    output_b9: bool,
 }
 
-impl<F: FieldExt> FromBinaryState<F> {
+impl<F: FieldExt> StateBaseConversion<F> {
     fn configure(
         meta: &mut ConstraintSystem<F>,
         state: [Column<Advice>; 25],
-        table: FromBinaryTableConfig<F>,
-        output_b9: bool,
+        bi: BaseInfo<F>,
     ) -> Self {
-        let bi = table.get_base_info(output_b9);
         let bccs: [BaseConversionConfig<F>; 25] = state
             .iter()
             .map(|&lane| {
@@ -32,64 +29,7 @@ impl<F: FieldExt> FromBinaryState<F> {
             .try_into()
             .unwrap();
 
-        Self {
-            table,
-            bccs,
-            state,
-            output_b9,
-        }
-    }
-
-    pub fn assign_region(
-        &self,
-        layouter: &mut impl Layouter<F>,
-        input_state: [F; 25],
-    ) -> Result<[F; 25], Error> {
-        let output_state: Result<Vec<F>, Error> = input_state
-            .iter()
-            .zip(self.bccs.iter())
-            .map(|(&lane, config)| {
-                let output = config.assign_region(layouter, lane)?;
-                Ok(output)
-            })
-            .into_iter()
-            .collect();
-        let output_state = output_state?;
-        let output_state: [F; 25] = output_state.try_into().unwrap();
-        Ok(output_state)
-    }
-}
-
-struct FromBase9State<F> {
-    table: FromBase9TableConfig<F>,
-    bccs: [BaseConversionConfig<F>; 25],
-    state: [Column<Advice>; 25],
-    output_b2: bool,
-}
-
-impl<F: FieldExt> FromBase9State<F> {
-    fn configure(
-        meta: &mut ConstraintSystem<F>,
-        state: [Column<Advice>; 25],
-        table: FromBase9TableConfig<F>,
-        output_b2: bool,
-    ) -> Self {
-        let bi = table.get_base_info(output_b2);
-        let bccs: [BaseConversionConfig<F>; 25] = state
-            .iter()
-            .map(|&lane| {
-                BaseConversionConfig::configure(meta, bi.clone(), lane)
-            })
-            .collect::<Vec<_>>()
-            .try_into()
-            .unwrap();
-
-        Self {
-            table,
-            bccs,
-            state,
-            output_b2,
-        }
+        Self { bi, bccs, state }
     }
 
     pub fn assign_region(
